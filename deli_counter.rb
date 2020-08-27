45520521874235dc63e01e95b3c3f9d0f1148cf6
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-27 23:23:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,8 +11,8 @@ def line (katz_deli)
 end
 
 def take_a_number (katz_deli,name)
-  
-  katz_deli << "Welcome, #{name}. You are number #{num+1}"
+  katz_deli=[]
+  katz_deli << "Welcome, #{name}. You are number #{i+1}"
 end
 
 def now_serving (katz_deli)
