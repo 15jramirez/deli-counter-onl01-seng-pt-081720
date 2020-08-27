4a49ef2169b2958ded6747f903b74f03a6fecece
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-27 22:57:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line (katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    string_line ="The line is currently:"
+    katz_deli.each_with_index do |name,num|
+       string_line += " #{num + 1}. #{name}"
+       
+     
+    end
+   puts string_line
+  end
+
+end
+
+def take_a_number (katz_deli,customer_name)
+  "Welcome, #{name}"
+  "You are number #{nme"
+end
+
+def now_serving (katz_deli)
+end
